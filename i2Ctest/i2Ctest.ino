64e6f7047667f442eac749da6a2aea90f232e6b5
DJMaltby <djtm@uw.edu>
2022-02-22 14:29:16 -0800

    i2Ctest displays bytes read (currnelty zero)

--- i2Ctest/i2Ctest.ino
+++ i2Ctest/i2Ctest.ino
@@ -1,26 +1,51 @@
+#include <LSM9DS1.h>
+
 #include <Wire.h>
 
-#define mainAddress 0x3C
+// one of these two (may be) the address for LSM9DS1
+// #define mainAddress 0x3C
+#define mainAddress 0x6B
+
+// x-axis gyroscope outputs
 #define OUT_X_L_G 0x18
 #define OUT_X_H_G 0x19
+
+// y-axis gyroscope outputs
 #define OUT_Y_L_G 0x1A
 #define OUT_Y_H_G 0x1B
+
+// z-axis gyroscope outputs
 #define OUT_Z_L_G 0x1C
 #define OUT_Z_H_G 0x1D
-#define CTRL_REG3_G 0x12
+
+// Control register 1 for gyroscope
+#define CTRL_REG1_G 0x10
 
 int gx, gy, gz, ax, ay, az;
 
 void setup() {
-  // put your setup code here, to run once:
   Wire.begin();
   Serial.begin(38400);
   delay(100);
 
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ"); 
+
   Wire.beginTransmission(mainAddress);
-  Wire.write(CTRL_REG3_G);
-  Wire.write(1<<7);
-  Wire.endTransmission();
+
+  // 119 Hz, 2000 dps, 16 Hz BW
+  Wire.write(CTRL_REG1_G);
+  Wire.write(78);
+  
+  Wire.endTransmission(); 
 }
 
 void loop() {
@@ -34,7 +59,10 @@ void loop() {
 
   Wire.requestFrom(mainAddress, 3);
 
-  if (Wire.available() <= 3) {
+  Serial.print("bytes read: ");
+  Serial.println(Wire.available());
+
+  if (3 >= Wire.available()) {
     gx = Wire.read();
     gy = Wire.read();
     gz = Wire.read();
